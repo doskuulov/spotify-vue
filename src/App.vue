37a5b80a8cb9a0124a910b2b2e9dba30e4b04f81
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-02-26 23:41:26 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+	<div class="bg-red-500">dwqefwrsqwdefagetr</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<script setup></script>
+
+<template>fAVSE</template>
